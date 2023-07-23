93128ebc0b9b3b54bc3aee3e0f8244280441f4d0
Arijeet De <arijeetde@gmail.com>
2023-07-23 21:06:21 +0530

    adding buttons and preload

--- main.js
+++ main.js
@@ -1,18 +1,14 @@
-console.log("Hello  World!");
+// const { PDFDocument} = require("pdf-lib");
+// const { readFileSync } = require("fs");
 
-const { PDFDocument} = require("pdf-lib");
-const { readFileSync } = require("fs");
+// async function getNumPages() {
+//   const pdfPath = "D:\\Semesters\\Semester_VIII\\MIDSEM_Exam_Prog.pdf";
+//   const document = await PDFDocument.load(readFileSync(pdfPath));
+//   const totalPages = document.getPageCount();
+//   console.log(totalPages);
+// }
 
-async function getNumPages() {
-  const pdfPath = "D:\\Semesters\\Semester_VIII\\MIDSEM_Exam_Prog.pdf";
-  const document = await PDFDocument.load(readFileSync(pdfPath));
-  const totalPages = document.getPageCount();
-  console.log(totalPages);
-}
-
-getNumPages();
-
-
+// getNumPages();
 
 
 
@@ -29,3 +25,60 @@ getNumPages();
 //   // const mergedPdfBuffer = await merger.saveAsBuffer();
 //   // fs.writeSync('merged.pdf', mergedPdfBuffer);
 // })();
+
+
+const path = require('path');
+
+const {app, BrowserWindow, Menu, ipcMain, shell} = require('electron');
+
+const isDev = process.env.NODE_ENV !== 'production';
+const isMac = process.platform === 'darwin';
+
+let mainWindow;
+
+// Create Main Window
+function createMainWindow() {
+    mainWindow = new BrowserWindow({
+        title: 'Image Resizer',
+        width: isDev ? 1200 : 700,
+        // width: 500,
+        height: 600,
+        webPreferences: {
+            contextIsolation: true,
+            nodeIntegration: true,
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    // Open devtools if in dev env
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
+}
+
+// App is ready
+app.whenReady().then(() => {
+  createMainWindow();
+
+  // // Implement Menu
+  // const mainMenu = Menu.buildFromTemplate(menu);
+  // Menu.setApplicationMenu(mainMenu);
+
+  // Remove mainWindow from memory on close
+  mainWindow.on('closed', () => (mainWindow = null));
+
+  app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) {
+          createMainWindow();
+      }
+  });
+});
+
+// App closed
+app.on('window-all-closed', () => {
+  if (!isMac) {
+      app.quit();
+  }
+});
