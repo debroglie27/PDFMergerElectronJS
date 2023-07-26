13c7b86273d7ce85bea4b1a1e39a470fb01a022a
Arijeet De <arijeetde@gmail.com>
2023-07-26 12:31:56 +0530

    simple bug fix and increase page num

--- renderer/js/pageContainerButton.js
+++ renderer/js/pageContainerButton.js
@@ -2,26 +2,53 @@ export { decreasePageNum, increasePageNum };
 
 
 function decreasePageNum (event) {
-    const parentClassList = event.target.parentElement.classList;
+    const button = event.currentTarget;
+    const parentClassList = button.parentElement.classList;
 
+    // Button belongs to first page container
     if ([...parentClassList].includes('first-page-container')) {
-        const firstPage = event.target.parentElement.querySelector('.first-page');
+        const firstPage = button.parentElement.querySelector('.first-page');
 
+        // Making sure first page does not become less than 1
         if (Number(firstPage.textContent) > 1) {
             firstPage.textContent = Number(firstPage.textContent) - 1;
         }
     }
+    // Button belongs to last page container
     else {
-        const lastPage = event.target.parentElement.querySelector('.last-page');
-        // const firstPageConst = event.target.parentElement.parentElement.querySelector('.first-page-const');
-        console.log(event.target.parentElement.parentElement.querySelector('.hidden .first-page-const'));
+        const lastPage = button.parentElement.querySelector('.last-page');
+        const firstPage = button.parentElement.parentElement.querySelector('.first-page');
 
-        // if (Number(lastPage.textContent) > Number(firstPageConst.textContent)) {
-        //     lastPage.textContent = Number(lastPage.textContent) - 1;
-        // }
+        // Making sure last page does not become smaller than first page
+        if (Number(lastPage.textContent) > Number(firstPage.textContent)) {
+            lastPage.textContent = Number(lastPage.textContent) - 1;
+        }
     }
 }
 
 function increasePageNum (event) {
+    // button element that was clicked
+    const button = event.currentTarget;
+    const parentClassList = button.parentElement.classList;
 
+    // Button belongs to first page container
+    if ([...parentClassList].includes('first-page-container')) {
+        const firstPage = button.parentElement.querySelector('.first-page');
+        const lastPage = button.parentElement.parentElement.querySelector('.last-page');
+
+        // Making sure first page does not become larger than last page
+        if (Number(firstPage.textContent) < Number(lastPage.textContent)) {
+            firstPage.textContent = Number(firstPage.textContent) + 1;
+        }
+    }
+    // Button belongs to last page container
+    else {
+        const lastPage = button.parentElement.querySelector('.last-page');
+        const lastPageConst = button.parentElement.parentElement.querySelector('.last-page-const');
+
+        // Making sure last page does not become larger than last page of pdf
+        if (Number(lastPage.textContent) < Number(lastPageConst.textContent)) {
+            lastPage.textContent = Number(lastPage.textContent) + 1;
+        }
+    }
 }
